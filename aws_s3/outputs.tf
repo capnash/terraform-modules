1b9e5d68c4e33164ab1e10f997f6048034c3b56d
Philip Davies <me@pdavies.io>
2020-10-13 14:21:15 +0100

    making outputs not be list

--- aws_s3/outputs.tf
+++ aws_s3/outputs.tf
@@ -1,23 +1,23 @@
 output "kms_s3_bucket_id" {
-  value = [aws_s3_bucket.kms_bucket[*].id]
+  value = aws_s3_bucket.kms_bucket[0].id
 }
 
 output "kms_s3_bucket_arn" {
-  value = [aws_s3_bucket.kms_bucket[*].arn]
+  value = aws_s3_bucket.kms_bucket[0].arn
 }
 
 output "kms_s3_bucket_region" {
-  value = [aws_s3_bucket.kms_bucket[*].region]
+  value = aws_s3_bucket.kms_bucket[0].region
 }
 
 output "s3_bucket_id" {
-  value = [aws_s3_bucket.bucket[*].id]
+  value = aws_s3_bucket.bucket[0].id
 }
 
 output "s3_bucket_arn" {
-  value = [aws_s3_bucket.bucket[*].arn]
+  value = aws_s3_bucket.bucket[0].arn
 }
 
 output "s3_bucket_region" {
-  value = [aws_s3_bucket.bucket[*].region]
+  value = aws_s3_bucket.bucket[0].region
 }
