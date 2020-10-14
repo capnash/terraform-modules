4d55b960b372b48159c841a954e5bb878fa60d26
Philip Davies <me@pdavies.io>
2020-10-14 15:45:12 +0100

    updated outputs

--- aws_s3/outputs.tf
+++ aws_s3/outputs.tf
@@ -1,23 +1,23 @@
 output "kms_s3_bucket_id" {
-  value = [aws_s3_bucket.kms_bucket[*].id]
+  value = join(",", aws_s3_bucket.kms_bucket[*].id)
 }
 
 output "kms_s3_bucket_arn" {
-  value = [aws_s3_bucket.kms_bucket[*].arn]
+  value = join(",", aws_s3_bucket.kms_bucket[*].arn)
 }
 
 output "kms_s3_bucket_region" {
-  value = [aws_s3_bucket.kms_bucket[*].region]
+  value = join(",", aws_s3_bucket.kms_bucket[*].region)
 }
 
 output "s3_bucket_id" {
-  value = [aws_s3_bucket.bucket[*].id]
+  value = join(",", aws_s3_bucket.bucket[*].id)
 }
 
 output "s3_bucket_arn" {
-  value = [aws_s3_bucket.bucket[*].arn]
+  value = join(",", aws_s3_bucket.bucket[*].arn)
 }
 
 output "s3_bucket_region" {
-  value = [aws_s3_bucket.bucket[*].region]
+  value = join(",", aws_s3_bucket.bucket[*].region)
 }
