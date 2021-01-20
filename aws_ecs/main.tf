04718766b350dbbd32fa7b9579cb9d3814ec6e7e
Philip Davies <me@pdavies.io>
2021-01-20 22:05:12 +0000

    adding the aws_alb_tg module

--- aws_ecs/main.tf
+++ aws_ecs/main.tf
@@ -5,19 +5,19 @@ data "aws_vpc_endpoint_service" "s3" {
 }
 
 # data "template_file" "user_data" {
-#   template = file("./files/user_data.sh")
+#   template = file(""${path.module}/files/user_data.sh"})
 
 #   vars = 
 # }
 
 data "archive_file" "slack_notifications" {
   type        = "zip"
-  source_file = "./files/slack_notification/snsToSlack.js"
-  output_path = "./files/slack_notification/snsToSlack.js.zip"
+  source_file = "${path.module}/files/slack_notification/snsToSlack.js}"
+  output_path = "${path.module}/files/slack_notification/snsToSlack.js.zip}"
 }
 
 data "archive_file" "ecs_lifecycle_function" {
   type        = "zip"
-  source_file = "./files/ecs_lifecycle_function/index.py"
-  output_path = "./files/ecs_lifecycle_function/index.py.zip"
+  source_file = "${path.module}/files/ecs_lifecycle_function/index.py}"
+  output_path = "${path.module}/files/ecs_lifecycle_function/index.py.zip}"
 }
