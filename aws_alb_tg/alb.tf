cf99c44e2735cc2e255dd05a6e50552f6b779825
Philip Davies <me@pdavies.io>
2021-01-20 20:56:01 +0000

    adding the aws_alb_tg module

--- aws_alb_tg/alb.tf
+++ aws_alb_tg/alb.tf
@@ -24,25 +24,25 @@ resource "aws_lb" "alb" {
 ## Outputs ##
 #############
 output "alb_id" {
-   value = "${aws_lb.alb.id}"
+   value = aws_lb.alb.id
 }
 
 output "alb_name" {
-   value = "${aws_lb.alb.name}"
+   value = aws_lb.alb.name
 }
 
 output "alb_arn" {
-   value = "${aws_lb.alb.arn}"
+   value = aws_lb.alb.arn
 }
 
 output "alb_arn_suffix" {
-   value = "${aws_lb.alb.arn_suffix}"
+   value = aws_lb.alb.arn_suffix
 }
 
 output "alb_dns_name" {
-   value = "${aws_lb.alb.dns_name}"
+   value = aws_lb.alb.dns_name
 }
 
 output "alb_zone_id" {
-   value = "${aws_lb.alb.zone_id}"
+   value = aws_lb.alb.zone_id
 }
