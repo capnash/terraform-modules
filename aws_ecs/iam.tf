a9edf5792ca19704e38bbe09d9078519dedb90c8
Philip Davies <me@pdavies.io>
2021-01-21 09:06:46 +0000

    adding the aws_alb_tg module

--- aws_ecs/iam.tf
+++ aws_ecs/iam.tf
@@ -29,10 +29,10 @@ resource "aws_iam_role_policy_attachment" "ecs_policy_attach" {
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_ssm_policy_attach" {
-    role       = aws_iam_role.role.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleForSSM"
-}
+# resource "aws_iam_role_policy_attachment" "ecs_ssm_policy_attach" {
+#     role       = aws_iam_role.role.name
+#     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleForSSM"
+# }
 
 resource "aws_iam_role_policy_attachment" "ecs_ec2_cloudwatch_role" {
   role       = aws_iam_role.role.name
