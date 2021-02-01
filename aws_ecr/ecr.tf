ca38aa741c323d33bbb5c56b287ba37abf607026
Anish Bhattachan <anish@LAPTOP01092.local>
2021-02-01 17:10:09 +0000

    update the module for ecr

--- aws_ecr/ecr.tf
+++ aws_ecr/ecr.tf
@@ -63,17 +63,17 @@ EOF
 #############
 
 output "ecr_arn" {
-  value = "${aws_ecr_repository.repo.*.arn}"
+  value = aws_ecr_repository.repo.arn
 }
 
 output "ecr_name" {
-  value = "${aws_ecr_repository.repo.*.name}"
+  value = aws_ecr_repository.repo.name
 }
 
 output "ecr_registry_id" {
-  value = "${aws_ecr_repository.repo.*.registry_id}"
+  value = aws_ecr_repository.repo.registry_id
 }
 
 output "ecr_repository_url" {
-  value = "${aws_ecr_repository.repo.*.repository_url}"
+  value = aws_ecr_repository.repo.repository_url
 }
