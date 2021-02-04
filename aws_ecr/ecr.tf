eff556bd2867cbb8b0dd9cbe3b8b5515ff5e0e77
Anish Bhattachan <31078735+antarmy07@users.noreply.github.com>
2021-02-04 09:18:52 +0000

    Adding AWS ECR - Module from old to new (#7)
    * adding aws ecr module from old to new
    
    * adding aws ecr module from old to new
    
    * adding aws ecr module from old to new
    
    * update the module for ecr
    
    * update the module for ecr
    
    * update the module for ecr
    
    * fixing ecr permission
    
    * fixing ecr permission
    
    * fixing ecr permission
    
    * fixing ecr permission
    
    * fixing context env
    
    Co-authored-by: Anish Bhattachan <anish@LAPTOP01092.local>
    Co-authored-by: Anish Bhattachan <anish@LAPTOP01092.home>

---
+++ aws_ecr/ecr.tf
@@ -0,0 +1,76 @@
+resource "aws_ecr_repository" "repo" {
+  name  = var.repo_name
+  tags  = var.tags
+}
+
+resource "aws_ecr_repository_policy" "repo_policy" {
+  repository = aws_ecr_repository.repo.name
+
+  policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+      {
+          "Sid": "AllowPushPull",
+          "Effect": "Allow",
+          "Principal":{
+                "AWS": "arn:aws:iam::${var.account_id}:root"
+          },
+          "Action": [
+              "ecr:BatchCheckLayerAvailability",
+              "ecr:BatchGetImage",
+              "ecr:CompleteLayerUpload",
+              "ecr:GetDownloadUrlForLayer",
+              "ecr:InitiateLayerUpload",
+              "ecr:PutImage",
+              "ecr:UploadLayerPart"
+          ]
+      }
+  ]
+}
+EOF
+}
+
+resource "aws_ecr_lifecycle_policy" "repo_policy" {
+  repository = aws_ecr_repository.repo.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": ${var.lc_rule_priority},
+      "description": "Expire untagged images older than ${var.lc_expire_days} days",
+      "selection": {
+        "tagStatus": "untagged",
+        "countType": "sinceImagePushed",
+        "countUnit": "days",
+        "countNumber": ${var.lc_expire_days}
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
+#############
+## Outputs ##
+#############
+
+output "ecr_arn" {
+  value = aws_ecr_repository.repo.arn
+}
+
+output "ecr_name" {
+  value = aws_ecr_repository.repo.name
+}
+
+output "ecr_registry_id" {
+  value = aws_ecr_repository.repo.registry_id
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.repo.repository_url
+}
---
+++ aws_ecr/variables.tf
@@ -0,0 +1,31 @@
+variable "repo_name" {
+  description = "Name of Docker Repo"
+  type        = "string"
+}
+
+variable "lc_rule_priority" {
+  description = "Life Cycle Rule priority"
+  type        = "string"
+  default     = 1
+}
+
+variable "lc_expire_days" {
+  description = "Life Cycle Rule - number of days to expirer untagged image sinceImagePushed"
+  type        = "string"
+  default     = 14
+}
+
+variable "account_id" {
+  description = "The aws account id that will have access to the repo"
+  type        = "string"
+}
+
+variable "enabled" {
+  description = "service enabled"
+  default     = "true"
+}
+
+variable "tags" {
+  description = "map of tags"
+  type        = "map"
+}
